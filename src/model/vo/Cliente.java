814c6db1591e0dce2a1fe6fee22eb3deab7c8bbb
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-17 21:31:14 -0300

    feat: add client model and properties

---
+++ src/model/vo/Cliente.java
@@ -0,0 +1,119 @@
+package model.vo;
+
+import java.time.LocalDate;
+
+public class Cliente {
+	
+	private Integer id;
+	private String nome;
+	private String cpf;
+	private String dataNascimento;
+	private String telefone;
+	private String email;
+	private String rua;
+	private String bairro;
+	private String cep;
+	private String estado;
+	
+	public Cliente(Integer id, String nome, String cpf, String dataNascimento, String telefone, String email,
+			String rua, String bairro, String cep, String estado) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.telefone = telefone;
+		this.email = email;
+		this.rua = rua;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.estado = estado;
+	}
+
+	public Cliente() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+}

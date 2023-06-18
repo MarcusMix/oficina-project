6e84a32148f97f3a62eb020689a3aebff2a1634f
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-17 21:30:25 -0300

    feat: methods to validate info and create client add cep

--- src/view/CadastroCliente.java
+++ src/view/CadastroCliente.java
@@ -5,11 +5,13 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import controller.ClienteController;
+import model.vo.Cliente;
 
 import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
 import java.awt.event.ActionEvent;
 
 public class CadastroCliente extends JPanel {
@@ -19,7 +21,7 @@ public class CadastroCliente extends JPanel {
 	private JTextField inputTelefone;
 	private JTextField inputEmail;
 	private JTextField inputRua;
-	private JTextField inputBairro;
+	private JTextField inputCEP;
 	private JTextField inputModelo;
 	private JTextField inputMarca;
 	private JTextField inputAno;
@@ -32,7 +34,7 @@ public class CadastroCliente extends JPanel {
 	private JLabel lblTelefone;
 	private JLabel lblEmail;
 	private JLabel lblRua;
-	private JLabel lblBairro;
+	private JLabel lblCEP;
 	private JLabel lblEstado;
 	private JLabel lblMarca;
 	private JLabel lblAno;
@@ -43,125 +45,152 @@ public class CadastroCliente extends JPanel {
 	public CadastroCliente() {
 		setLayout(null);
 		setBounds(100, 100, 746, 412);
-		
+
 		lblNome = new JLabel("Nome:");
-		lblNome.setBounds(10, 75, 46, 14);
+		lblNome.setBounds(29, 72, 46, 14);
 		add(lblNome);
-		
+
 		inputNome = new JTextField();
 		inputNome.setBounds(115, 72, 210, 20);
 		add(inputNome);
 		inputNome.setColumns(10);
-		
+
 		lblNewLabel = new JLabel("Cadastro de Cliente");
 		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 24));
 		lblNewLabel.setBounds(238, 22, 239, 28);
 		add(lblNewLabel);
-		
+
 		lblCpf = new JLabel("CPF:");
-		lblCpf.setBounds(10, 109, 46, 14);
+		lblCpf.setBounds(29, 106, 46, 14);
 		add(lblCpf);
-		
+
 		inputCpf = new JTextField();
 		inputCpf.setColumns(10);
 		inputCpf.setBounds(115, 103, 210, 20);
 		add(inputCpf);
-		
+
 		lblDataNascimento = new JLabel("Data Nasc:");
-		lblDataNascimento.setBounds(10, 141, 55, 14);
+		lblDataNascimento.setBounds(29, 138, 55, 14);
 		add(lblDataNascimento);
-		
+
 		inputDataNasc = new JTextField();
 		inputDataNasc.setColumns(10);
 		inputDataNasc.setBounds(115, 138, 210, 20);
 		add(inputDataNasc);
-		
+
 		lblTelefone = new JLabel("Telefone");
-		lblTelefone.setBounds(10, 176, 55, 14);
+		lblTelefone.setBounds(29, 173, 55, 14);
 		add(lblTelefone);
-		
+
 		lblEmail = new JLabel("E-mail:");
-		lblEmail.setBounds(10, 201, 55, 14);
+		lblEmail.setBounds(29, 198, 55, 14);
 		add(lblEmail);
-		
+
 		lblRua = new JLabel("Rua:");
-		lblRua.setBounds(10, 236, 55, 14);
+		lblRua.setBounds(29, 233, 55, 14);
 		add(lblRua);
-		
-		lblBairro = new JLabel("Bairro:");
-		lblBairro.setBounds(10, 261, 55, 14);
-		add(lblBairro);
-		
+
+		lblCEP = new JLabel("Bairro:");
+		lblCEP.setBounds(29, 296, 55, 14);
+		add(lblCEP);
+
 		lblEstado = new JLabel("Estado:");
-		lblEstado.setBounds(10, 295, 55, 14);
+		lblEstado.setBounds(29, 265, 55, 14);
 		add(lblEstado);
-		
+
 		inputTelefone = new JTextField();
 		inputTelefone.setColumns(10);
 		inputTelefone.setBounds(115, 173, 210, 20);
 		add(inputTelefone);
-		
+
 		inputEmail = new JTextField();
 		inputEmail.setColumns(10);
 		inputEmail.setBounds(115, 198, 210, 20);
 		add(inputEmail);
-		
+
 		inputRua = new JTextField();
 		inputRua.setColumns(10);
 		inputRua.setBounds(115, 233, 210, 20);
 		add(inputRua);
-		
-		inputBairro = new JTextField();
-		inputBairro.setColumns(10);
-		inputBairro.setBounds(115, 258, 210, 20);
-		add(inputBairro);
-		
+
+		inputCEP = new JTextField();
+		inputCEP.setColumns(10);
+		inputCEP.setBounds(115, 296, 210, 20);
+		add(inputCEP);
+
 		lblModeloCarro = new JLabel("Modelo Carro:");
-		lblModeloCarro.setBounds(431, 75, 46, 14);
+		lblModeloCarro.setBounds(431, 75, 68, 14);
 		add(lblModeloCarro);
-		
+
 		lblMarca = new JLabel("Marca:");
 		lblMarca.setBounds(431, 109, 46, 14);
 		add(lblMarca);
-		
+
 		lblAno = new JLabel("Ano:");
 		lblAno.setBounds(431, 141, 46, 14);
 		add(lblAno);
-		
+
 		lblPlaca = new JLabel("Placa:");
 		lblPlaca.setBounds(431, 176, 46, 14);
 		add(lblPlaca);
-		
-		comboBoxEstado = new JComboBox();
-		comboBoxEstado.setBounds(115, 291, 210, 22);
+
+		String[] estados = {
+				"Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", 
+				"Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais",
+				"Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", 
+				"Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
+		};
+
+		comboBoxEstado = new JComboBox(estados);
+		comboBoxEstado.setBounds(115, 264, 210, 22);
 		add(comboBoxEstado);
-		
+
 		inputModelo = new JTextField();
 		inputModelo.setColumns(10);
-		inputModelo.setBounds(483, 72, 210, 20);
+		inputModelo.setBounds(509, 72, 210, 20);
 		add(inputModelo);
-		
+
 		inputMarca = new JTextField();
 		inputMarca.setColumns(10);
-		inputMarca.setBounds(483, 106, 210, 20);
+		inputMarca.setBounds(509, 106, 210, 20);
 		add(inputMarca);
-		
+
 		inputAno = new JTextField();
 		inputAno.setColumns(10);
-		inputAno.setBounds(483, 138, 210, 20);
+		inputAno.setBounds(509, 138, 210, 20);
 		add(inputAno);
-		
+
 		inputPlaca = new JTextField();
 		inputPlaca.setColumns(10);
-		inputPlaca.setBounds(483, 173, 210, 20);
+		inputPlaca.setBounds(509, 173, 210, 20);
 		add(inputPlaca);
-		
+
 		btnNewButton = new JButton("Cadastrar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// lógica de chamar o controller
-				ClienteController.cadastrarCliente();
+				//criar cliente com os dados digitados
+				Cliente novoCliente = new Cliente();
+				novoCliente.setNome(inputNome.getText());
+				novoCliente.setCpf(inputCpf.getText());
+				novoCliente.setDataNascimento(inputDataNasc.getText());
+				novoCliente.setBairro(inputCEP.getText());
+				novoCliente.setEmail(inputEmail.getText());
+				novoCliente.setRua(inputRua.getText());
+				novoCliente.setEstado((String) comboBoxEstado.getSelectedItem());
+				novoCliente.setTelefone(inputTelefone.getText());
+				novoCliente.setCep(inputCEP.getText());
+				
+				//criar automovel
+
+				//verificar campos obrigatorios
+				if(ClienteController.verificarCamposObrigatorios(novoCliente)) {
+					// lógica de chamar o controller
+					ClienteController.cadastrarCliente(novoCliente);
+				}
 			}
+
+
+
 		});
 		btnNewButton.setBounds(539, 378, 89, 23);
 		add(btnNewButton);

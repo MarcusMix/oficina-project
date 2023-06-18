d4067f9151ff87ea7a11723030612e03b7dfa614
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-17 21:30:49 -0300

    feat: validate info of client

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,10 +1,60 @@
 package controller;
 
-public class ClienteController {
+import javax.swing.JOptionPane;
 
-	public static void cadastrarCliente() {
+import model.vo.Cliente;
+
+public class ClienteController {
+	
+	public static boolean verificarCamposObrigatorios(Cliente novoCliente) {
+		if(novoCliente.getNome().isBlank()) {
+			JOptionPane.showMessageDialog(null, "Cliente em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getCpf().isBlank()) {
+			JOptionPane.showMessageDialog(null, "CPF em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getDataNascimento().isBlank()) {
+			JOptionPane.showMessageDialog(null, "Data de Nascimento em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getTelefone().isBlank()) {
+			JOptionPane.showMessageDialog(null, "Telefone em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getEmail().isBlank()) {
+			JOptionPane.showMessageDialog(null, "E-mail em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getRua().isBlank()) {
+			JOptionPane.showMessageDialog(null, "Rua em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getBairro().isBlank()) {
+			JOptionPane.showMessageDialog(null, "Bairro em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getCep().isBlank()) {
+			JOptionPane.showMessageDialog(null, "CEP em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else if(novoCliente.getEstado().isBlank()) {
+			JOptionPane.showMessageDialog(null, "Estado em branco!", 
+					"Erro", JOptionPane.ERROR_MESSAGE);
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public static void cadastrarCliente(Cliente novoCliente) {
 		
 		
 	}
+
+	
+
+	
 	
 }

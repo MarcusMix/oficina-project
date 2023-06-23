0fdac6babc594e8f09c5184ef2c217c8f0b5d2be
Marcus Sandi <tradedomarcus@gmail.com>
2023-06-23 07:40:08 -0300

    feat: insert into db cliente

--- src/model/dao/ClienteDAO.java
+++ src/model/dao/ClienteDAO.java
@@ -11,25 +11,21 @@ import model.vo.Cliente;
 public class ClienteDAO {
 
 	public static void cadastrarClienteDAO(Cliente novoCliente) {
-		String query = "INSERT INTO clientes (nome, cpf, dataNascimento, email, telefone, "
-				+ "rua, bairro, cep, estado) VALUES (?, ?, ? ,? ,? ,? ,? ,?, ?)";
+		String query = "INSERT INTO cliente (rua, bairro, cep, estado) VALUES (?, ?, ? ,?)";
+		System.out.println(query);
 		Connection conn = Banco.getConnection();
 		PreparedStatement pstmt = Banco.getPreparedStatementWithPk(conn, query);
 		
 		try {
-			pstmt.setString(1, novoCliente.getNome());
-			pstmt.setString(2, novoCliente.getCpf());
-			pstmt.setString(3, novoCliente.getDataNascimento());
-			pstmt.setString(4, novoCliente.getEmail());
-			pstmt.setString(5, novoCliente.getRua());
-			pstmt.setString(6, novoCliente.getBairro());
-			pstmt.setString(7, novoCliente.getCep());
-			pstmt.setString(8, novoCliente.getEstado());
+			pstmt.setString(1, novoCliente.getRua());
+			pstmt.setString(2, novoCliente.getBairro());
+			pstmt.setString(3, novoCliente.getCep());
+			pstmt.setString(4, novoCliente.getEstado());
 			
 			pstmt.execute();
 			ResultSet resultado = pstmt.getGeneratedKeys(); 
 			if (resultado.next()) {
-				novoCliente.setId(resultado.getInt(1));
+				novoCliente.setIdCliente(resultado.getInt(1));
 			}
 			
 		} catch (SQLException erro){

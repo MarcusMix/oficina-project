d39c1b77e1f6de72094a3fcf0a06fabab6f2a52b
Marcus Sandi <tradedomarcus@gmail.com>
2023-07-01 19:42:44 -0300

    feat: validade data of pro and invokes method in model dao

--- src/controller/ProfissionalController.java
+++ src/controller/ProfissionalController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javax.swing.JOptionPane;
 
+import model.dao.PessoaDAO;
 import model.dao.ProfissionalDAO;
 import model.vo.Pessoa;
 import model.vo.Profissional;
@@ -9,15 +10,21 @@ import model.vo.Profissional;
 public class ProfissionalController {
 
 	public String validarDadosProfissional(Profissional novoProfissional) {
-		//TODO
+		String mensagemValidacao = "";
 		if(novoProfissional.getFuncao().isBlank()) {
-			JOptionPane.showMessageDialog(null, "Campo função é obrigatório!", 
-					"Erro", JOptionPane.ERROR);
-		} else {
-			ProfissionalDAO profissionalDAO = new ProfissionalDAO();
-//			profissionalDAO.cadastrarProfissional(novoProfissional);
-		}
-		return null;
+			mensagemValidacao += "Função inválida!";
+		} 
+		return mensagemValidacao;
 	}
+
+	public void cadastrarProfissional(Profissional novoProfissional) {
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		pessoaDAO.cadastrarPessoaDAO(novoProfissional);
+		
+		ProfissionalDAO profissionalDAO = new ProfissionalDAO();
+		profissionalDAO.cadastrarProfissionalDAO(novoProfissional);
+	}
+
+	
 
 }

a88834af468ab8a8f22dc4430bba4f341420aa13
Marcus Sandi <tradedomarcus@gmail.com>
2023-07-01 19:44:48 -0300

    refactor: add id pro

--- src/model/vo/Profissional.java
+++ src/model/vo/Profissional.java
@@ -1,35 +1,45 @@
 package model.vo;
 
 public class Profissional extends Pessoa{
+	private int idProfissional;
 	private String funcao;
 	private String dtAdimissao;
-	
+
+	public int getIdProfissional() {
+		return idProfissional;
+	}
+
+	public void setIdProfissional(int idProfissional) {
+		this.idProfissional = idProfissional;
+	}
+
+
 	public Profissional() {
 		super();
 	}
-	
+
 	public Profissional(String funcao, String dtAdimissao) {
 		super();
 		this.funcao = funcao;
 		this.dtAdimissao = dtAdimissao;
 	}
-	
+
 	public String getFuncao() {
 		return funcao;
 	}
-	
+
 	public void setFuncao(String funcao) {
 		this.funcao = funcao;
 	}
-	
+
 	public String getDtAdimissao() {
 		return dtAdimissao;
 	}
-	
+
 	public void setDtAdimissao(String dtAdimissao) {
 		this.dtAdimissao = dtAdimissao;
 	}
-	
 
-	
+
+
 }

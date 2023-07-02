25c893aa6b0ab945144be1e88ddc88820868f802
Marcus Sandi <tradedomarcus@gmail.com>
2023-07-02 18:32:41 -0300

    refactor: change date to local date

--- src/model/vo/Cliente.java
+++ src/model/vo/Cliente.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class Cliente extends Pessoa{
 	
 	private Integer idCliente;
+	private Pessoa pessoa;
 	private String rua;
 	private String bairro;
 	private String cep;
@@ -14,59 +15,41 @@ public class Cliente extends Pessoa{
 		super();
 	}
 	
-	public Cliente(int idpessoa, String nome, String cpf, String dtNascimento, String telefone, String email) {
-		super(idpessoa, nome, cpf, dtNascimento, telefone, email);
-		
-	}
-	
-	public Cliente(Integer idCliente, String rua, String bairro, String cep, String estado) {
-		super();
-		this.idCliente = idCliente;
-		this.rua = rua;
-		this.bairro = bairro;
-		this.cep = cep;
-		this.estado = estado;
-	}
-	
 	public Integer getIdCliente() {
 		return idCliente;
 	}
-	
 	public void setIdCliente(Integer idCliente) {
 		this.idCliente = idCliente;
 	}
-	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
 	public String getRua() {
 		return rua;
 	}
-	
 	public void setRua(String rua) {
 		this.rua = rua;
 	}
-	
 	public String getBairro() {
 		return bairro;
 	}
-	
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-	
 	public String getCep() {
 		return cep;
 	}
-	
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-	
 	public String getEstado() {
 		return estado;
 	}
-	
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-	
-	
+
 }

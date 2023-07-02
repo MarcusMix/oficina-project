5bd66a74bd97f1d041bcaf7c1cda9c887f51dc66
Marcus Sandi <tradedomarcus@gmail.com>
2023-07-02 18:32:47 -0300

    refactor: change date to local date

--- src/model/vo/Pessoa.java
+++ src/model/vo/Pessoa.java
@@ -1,25 +1,17 @@
 package model.vo;
 
+import java.time.LocalDate;
+
 public class Pessoa {
 	private int idpessoa;
     private String nome;
     private String cpf;
-    private String dtNascimento;
+    private LocalDate dtNascimento;
     private String telefone;
     private String email;
-    
+
 	public Pessoa() {
 		super();
-	}
-	
-	public Pessoa(int idpessoa, String nome, String cpf, String dtNascimento, String telefone, String email) {
-		super();
-		this.idpessoa = idpessoa;
-		this.nome = nome;
-		this.cpf = cpf;
-		this.dtNascimento = dtNascimento;
-		this.telefone = telefone;
-		this.email = email;
 	}
 
 	public int getIdpessoa() {
@@ -46,11 +38,11 @@ public class Pessoa {
 		this.cpf = cpf;
 	}
 
-	public String getDtNascimento() {
+	public LocalDate getDtNascimento() {
 		return dtNascimento;
 	}
 
-	public void setDtNascimento(String dtNascimento) {
+	public void setDtNascimento(LocalDate dtNascimento) {
 		this.dtNascimento = dtNascimento;
 	}
 
@@ -70,6 +62,4 @@ public class Pessoa {
 		this.email = email;
 	}
     
-    
-
 }

47f4a3eb01430012f6a0468f8ec650072a196ed8
Marcus Sandi <tradedomarcus@gmail.com>
2023-07-04 08:13:18 -0300

    feat: validade parameters to select correct sql query

--- src/model/dao/ClienteDAO.java
+++ src/model/dao/ClienteDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.vo.Automovel;
 import model.vo.Cliente;
@@ -36,6 +38,75 @@ public class ClienteDAO {
 			Banco.closePreparedStatement(pstmt);
 			Banco.closeConnection(conn);
 		}
+	}
+
+	public List<Cliente> consultarDAO(String nome, String cpf, String cidade) {
+		List<Cliente> clientes = new ArrayList<Cliente>();
+		Connection conexao = Banco.getConnection();
+		String sql = null;
+		
+		//buscar todos
+		if(nome.isBlank() && cpf.isBlank() && cidade.isBlank()) {
+			sql = "SELECT * FROM cliente";
+		}
+		
+		//buscar por nome
+		if(!nome.isBlank() && cpf.isBlank() && cidade.isBlank()) {
+			//TODO arrumar cpf (estado para testes)
+			sql = "SELECT * FROM cliente WHERE estado LIKE '" + nome + "%'";
+		}
+		
+		//buscar por cpf
+		if(nome.isBlank() && !cpf.isBlank() && cidade.isBlank()) {
+			sql = "SELECT * FROM cliente WHERE cpf LIKE '" + cpf + "%'";
+		}
+		
+		//buscar por cidade
+		if(nome.isBlank() && cpf.isBlank() && !cidade.isBlank()) {
+			sql = "SELECT * FROM cliente WHERE cidade LIKE '" + cidade + "%'";
+		}
+		
+		//buscar por nome e cpf
+		if(!nome.isBlank() && !cpf.isBlank() && cidade.isBlank()) {
+			sql = "SELECT * FROM cliente WHERE nome LIKE '" + nome + "%' AND cpf LIKE '" + cpf + "%'";
+		}
+		
+		//buscar por nome e cidade
+		if(!nome.isBlank() && cpf.isBlank() && !cidade.isBlank()) {
+			sql = "SELECT * FROM cliente WHERE nome LIKE '" + nome + "%' AND cidade LIKE '" + cidade + "%'";
+		}
+		
+		//buscar por cpf e cidade
+		if(nome.isBlank() && !cpf.isBlank() && !cidade.isBlank()) {
+			sql = "SELECT * FROM cliente WHERE cpf cidade '" + cidade + "%' AND cpf LIKE '" + cpf + "%'";
+		}
+		
+		System.out.println(sql);
+		PreparedStatement query = Banco.getPreparedStatement(conexao, sql);
+		
+		try {
+			ResultSet resultado = query.executeQuery();
+			while(resultado.next()) {
+				Cliente clienteConsultado = converterDeResultSetParaEntidade(resultado);
+				clientes.add(clienteConsultado);
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao buscar cliente pelo nome: " + e.getMessage());	
+		} finally {
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conexao);
+		}
+		
+		return clientes;
+	}
+	
+	private Cliente converterDeResultSetParaEntidade(ResultSet resultado) throws SQLException {
+		Cliente clienteConsultado = new Cliente(); 
+//		clienteConsultado.setNome(resultado.getString("nome"));
+		clienteConsultado.setCep(resultado.getString("cep"));
+		clienteConsultado.setEstado(resultado.getString("estado"));
+		clienteConsultado.setBairro(resultado.getString("bairro"));
+		return clienteConsultado;
 	}
 
 }
